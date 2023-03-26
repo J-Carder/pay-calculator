fabe4b2e6f5751537136cf4f272e1267a8683d32
JCarder <20077236+J-Carder@users.noreply.github.com>
2023-03-25 17:39:02 -0700

    changed outDir to public

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  publicDir: "static"
+  publicDir: "static",
+  build: {
+    outDir: "public"
+  }
 })
